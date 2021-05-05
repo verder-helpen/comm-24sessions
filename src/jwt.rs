1ac20bdfdd4e4c9698ac726cb79df7eaa326358f
Henk Dieter Oordt <henkdieter@oordt.net>
2021-05-05 11:08:57 +0200

    run cargo fmt

--- src/jwt.rs
+++ src/jwt.rs
@@ -20,10 +20,7 @@ pub enum JwtError {
 }
 
 /// Decrypt and verify a given jwe to extract the contained attributes.
-pub fn validate_jws(
-    jws: &str,
-    validator: &dyn JwsVerifier,
-) -> Result<JwtPayload, JwtError> {
+pub fn validate_jws(jws: &str, validator: &dyn JwsVerifier) -> Result<JwtPayload, JwtError> {
     let decoded_jws = jwt::decode_with_verifier(jws, validator)?.0;
     let mut validator = JwtPayloadValidator::new();
     validator.set_base_time(std::time::SystemTime::now());
@@ -39,11 +36,9 @@ pub fn claim_as_str<'a>(payload: &'a JwtPayload, key: &'static str) -> Result<&'
         .as_str()
         .ok_or(JwtError::InvalidFormat(key))
 }
+
 pub trait FromJws: Sized + DeserializeOwned {
-    fn from_jws(
-        jwt: &str,
-        validator: &dyn JwsVerifier,
-    ) -> Result<Self, JwtError> {
+    fn from_jws(jwt: &str, validator: &dyn JwsVerifier) -> Result<Self, JwtError> {
         let payload = validate_jws(jwt, validator)?;
         let payload = claim_as_str(&payload, "payload")?;
         let payload = serde_json::from_str(payload)?;
