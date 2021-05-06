ae352f9214c0d4cd6eab9b576ee486c0b02a00bf
Henk Dieter Oordt <henkdieter@oordt.net>
2021-05-06 20:29:18 +0200

    poll backend for any new attributes

--- attribute-ui/src/components/App.tsx
+++ attribute-ui/src/components/App.tsx
@@ -22,14 +22,19 @@ export const App = () => {
     const poll = () => fetch(`${serverUrl}/session_info/${hostToken}`).then(r => r.json())
 
     const [attrs, setAttrs] = useState<Attrubutes>(null);
-    useEffect(() => void poll().then(setAttrs), [])
-    if (!attrs) {
+
+    // Poll backend to check whether attributes have been received for current session
+    useEffect(() => {
+        poll().then(setAttrs);
+        const interval = setInterval(() => void poll().then(setAttrs), 3000);
+        return () => clearInterval(interval);
+    }, [])
+
+    if (!attrs || Object.keys(attrs).length === 0) {
         return <div>There are no identified guests yet</div>
     }  
     
-
     return (<>
-        
         {Object.entries(attrs).map(([name, {attributes}]) => {
             return <AttrCard key={name} name={name} attributes={attributes}/>
         })}
