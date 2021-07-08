3d80f7ffb94c2f8e554d49292de8ab75338d8076
Henk Dieter Oordt <hd@oordt.dev>
2021-07-08 17:12:40 +0200

    add README, add clarification to attribute-ui/config.js

--- attribute-ui/config.js
+++ attribute-ui/config.js
@@ -1,3 +1,5 @@
+// External URL for the 24Sessions ID Contact plugin, in order for the 
+// application to fetch the attributes.
 window.SERVER_URL = 'https://comm-24sessions.idcontact.test.tweede.golf'
 window.TRANSLATIONS = {
   nl: {
